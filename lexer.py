f065cad469d0a380dae1b5cfa7a45d155c41a22c
Fevret <efevret@gmail.Com>
2021-09-17 21:41:32 +0200

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,89 +1,63 @@
 from tokens import Token, TokenType
 
-WHITESPACE = ' \n\t'
-DIGITS = '1234567890'
-
+WHITESPACE = ' \t\n'
+DIGITS = '0123456789'
 
 class Lexer:
     def __init__(self, text):
         self.text = iter(text)
         self.advance()
-    
+
     def advance(self):
         try:
             self.current_char = next(self.text)
         except StopIteration:
             self.current_char = None
 
-
     def generate_tokens(self):
         while self.current_char != None:
             if self.current_char in WHITESPACE:
                 self.advance()
-            elif self.current_char == 'x':
-                yield self.generate_letter()
             elif self.current_char == '.' or self.current_char in DIGITS:
                 yield self.generate_number()
-            elif self.current_char == "+":
+            elif self.current_char == '+':
                 self.advance()
-                yield Token(TokenType.PLUS)
-            elif self.current_char == "-":
+                yield Token(TokenType.PLUS, '+')
+            elif self.current_char == '-':
                 self.advance()
-                yield Token(TokenType.MINUS)
-            elif self.current_char == "*":
+                yield Token(TokenType.MINUS, '-')
+            elif self.current_char == '*':
                 self.advance()
-                yield Token(TokenType.MULTIPLY)
-            elif self.current_char == "/":
+                yield Token(TokenType.MULTIPLY, '*')
+            elif self.current_char == '/':
                 self.advance()
-                yield Token(TokenType.DIVIDE)
-            elif self.current_char == "(":
+                yield Token(TokenType.DIVIDE, '/')
+            elif self.current_char == '(':
                 self.advance()
-                yield Token(TokenType.LPAREN)
-            elif self.current_char == ")":
+                yield Token(TokenType.LPAREN, '(')
+            elif self.current_char == ')':
                 self.advance()
-                yield Token(TokenType.RPAREN)
+                yield Token(TokenType.RPAREN, ')')
             else:
-                raise Exception(f"Charactere illégale")
-
-
-
-    def generate_letter(self):
-        decimal_point_count = 0
-        number_str = self.current_char
-        self.advance()
-
-        while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
-            if self.current_char == '.':
-                decimal_point_count += 1
-                if decimal_point_count > 1:
-                    break
+                raise Exception(f"Illegal character '{self.current_char}'")
                 
-            number_str += self.current_char
-            self.advance()
-
-        if number_str.endswith('x'):
-            number_str += number_str + "x"
-
-        return Token(TokenType.ALG, str(number_str))
-
-    def generate_number(self):
+	def generate_number(self):
         decimal_point_count = 0
-        number_str = self.current_char
-        self.advance()
+		number_str = self.current_char
+		self.advance()
 
-        while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
-            if self.current_char == '.':
-                decimal_point_count += 1
-                if decimal_point_count > 1:
-                    break
-                
-            number_str += self.current_char
-            self.advance()
+		while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
+			if self.current_char == '.':
+				decimal_point_count += 1
+				if decimal_point_count > 1:
+					break
+			
+			number_str += self.current_char
+			self.advance()
 
-        if number_str.startswith('.'):
-            number_str = '0' + number_str
-        
-        if number_str.endswith('.'):
-            number_str += '0'
+		if number_str.startswith('.'):
+			number_str = '0' + number_str
+		if number_str.endswith('.'):
+			number_str += '0'
 
-        return Token(TokenType.NUMBER, float(number_str))
+		return Token(TokenType.NUMBER, float(number_str))
