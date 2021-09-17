f3ede1c9bdf631ce5610a80f01f66225d886a915
Fevret <efevret@gmail.Com>
2021-09-17 21:44:26 +0200

    Fix bugs

--- lexer.py
+++ lexer.py
@@ -3,6 +3,7 @@ from tokens import Token, TokenType
 WHITESPACE = ' \t\n'
 DIGITS = '0123456789'
 
+
 class Lexer:
     def __init__(self, text):
         self.text = iter(text)
@@ -40,24 +41,22 @@ class Lexer:
                 yield Token(TokenType.RPAREN, ')')
             else:
                 raise Exception(f"Illegal character '{self.current_char}'")
-                
-	def generate_number(self):
+
+    def generate_number(self):
         decimal_point_count = 0
-		number_str = self.current_char
-		self.advance()
+        number_str = self.current_char
+        self.advance()
 
-		while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
-			if self.current_char == '.':
-				decimal_point_count += 1
-				if decimal_point_count > 1:
-					break
-			
-			number_str += self.current_char
-			self.advance()
+        while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
+            if self.current_char == '.':
+                decimal_point_count += 1
+                if decimal_point_count > 1:
+                    break
+            number_str += self.current_char
+            self.advance()
+        if number_str.startswith('.'):
+            number_str = '0' + number_str
+        if number_str.endswith('.'):
+            number_str += '0'
 
-		if number_str.startswith('.'):
-			number_str = '0' + number_str
-		if number_str.endswith('.'):
-			number_str += '0'
-
-		return Token(TokenType.NUMBER, float(number_str))
+        return Token(TokenType.NUMBER, float(number_str))
