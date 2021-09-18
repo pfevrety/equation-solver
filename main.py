ffb02c46edf275bb06f9644b8ee672ca7eb710d7
Fevret <efevret@gmail.Com>
2021-09-18 10:33:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from lexer import Lexer
 from parser_ import Parser
-
+from interpreter import Interpreter
 while True:
-    text = input("expression > ")
-    lexer = Lexer(text)
-    tokens = lexer.generate_tokens()
-    # print(list(tokens))  # [NUMBER.0: 1.0, PLUS.1, LITERAL.7 : 2]
-    parser = Parser(tokens)
-    tree = parser.parse()
-    print(tree)
+    try:
+        text = input("expression > ")
+        lexer = Lexer(text)
+        tokens = lexer.generate_tokens()
+        # print(list(tokens))  # [NUMBER.0: 1.0, PLUS.1, LITERAL.7 : 2]
+        parser = Parser(tokens)
+        tree = parser.parse()
+        if not tree:
+            continue
+        interpreter = Interpreter()
+        print(interpreter.visit(tree))
+        print(tree)
+    except Exception as e:
+        print(e)
