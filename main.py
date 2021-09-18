ec880c2cec80e798129e1900a2211d46d46d3bd4
Fevret <efevret@gmail.Com>
2021-09-18 09:33:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from lexer import Lexer
+from parser_ import Parser
 
 while True:
     text = input("expression > ")
     lexer = Lexer(text)
     tokens = lexer.generate_tokens()
-    print(list(tokens))
+    # print(list(tokens))  # [NUMBER.0: 1.0, PLUS.1, LITERAL.7 : 2]
+    parser = Parser(tokens)
+    tree = parser.parse()
+    print(tree)
