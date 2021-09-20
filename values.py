d8328f8939c867c907107f437533a68af5886075
Fevret <efevret@gmail.Com>
2021-09-20 20:39:45 +0200

    Update values.py

--- values.py
+++ values.py
@@ -6,3 +6,24 @@ class Number:
 
     def __repr__(self):
         return f"{self.value}"
+
+    def type(self):
+        return "NUMBER"
+
+# create literal class
+@dataclass
+class Literal:
+    value: list
+
+    def __repr__(self):
+        if self.value[0] != None and self.value[1] != None:
+            return f"{self.value[1]}x + {self.value[0]}"
+        if self.value[1] != None:
+            return f"{self.value[1]}x"
+        if self.value[0] != None:
+            return f"{self.value[0]}"
+
+        return f"{self.value}"
+
+    def type(self):
+        return "LITERAL"
