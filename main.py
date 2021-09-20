47ba93a7973ca07fced922dceea5a8c0eecdf975
Fevret <efevret@gmail.Com>
2021-09-20 20:39:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from lexer import Lexer
 from parser_ import Parser
 from interpreter import Interpreter
+
 while True:
-    try:
-        text = input("expression > ")
-        lexer = Lexer(text)
-        tokens = lexer.generate_tokens()
-        # print(list(tokens))  # [NUMBER.0: 1.0, PLUS.1, LITERAL.7 : 2]
-        parser = Parser(tokens)
-        tree = parser.parse()
-        if not tree:
-            continue
-        interpreter = Interpreter()
-        print(interpreter.visit(tree))
-        print(tree)
-    except Exception as e:
-        print(e)
+    # try:
+    text = input("expression > ")
+    lexer = Lexer(text)
+    tokens = lexer.generate_tokens()
+    # print(list(tokens))  # [NUMBER.0: 1.0, PLUS.1, LITERAL.7 : 2]
+    parser = Parser(tokens)
+    tree = parser.parse()
+    if not tree:
+        continue
+    interpreter = Interpreter()
+    print("Prioritées opératoires: " + str(tree))
+    r = str(interpreter.visit(tree))
+    print(r)
+    # print(f"Resultats:\n\ta: 0,\n\tb: {r[0]},\n\tc: {r[2]}")
+
+    
+
+
+    # except Exception as e:
+    #     print(e)
