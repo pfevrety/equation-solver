c14b5c5a5b1ac263f8fd7b5b1682dfbbe0efe85d
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-23 09:39:45 +0200

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,16 +1,14 @@
 from enum import Enum
 from dataclasses import dataclass
-class TokenType(Enum):
-    NUMBER = 0
-    PLUS = 1
-    MINUS = 2
-    MULTIPLY = 3
-    DIVIDE = 4
-    LPAREN = 5
-    RPAREN = 6
-    LITERAL = 7
-    EXPONENT = 8
 
+class TokenType(Enum):
+	NUMBER    = 0
+	PLUS      = 1
+	MINUS     = 2
+	MULTIPLY  = 3
+	DIVIDE    = 4
+	LPAREN    = 5
+	RPAREN    = 6
 
 @dataclass
 class Token:
@@ -18,4 +16,4 @@ class Token:
 	value: any = None
 
 	def __repr__(self):
-		return self.type.name + '.' + str(self.type.value) + (f" : {self.value}" if self.value != None else "")
+		return self.type.name + (f":{self.value}" if self.value != None else "")
