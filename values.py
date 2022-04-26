48fc5059c91f59d85fbbc5859fff9f5e0d9498dc
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-26 19:48:59 +0200

    Update values.py

--- values.py
+++ values.py
@@ -4,12 +4,19 @@ class Number:
     value: dict
 
     def __repr__(self):
-        if self.value['variables'] is None:
-            return f"{self.value['constant']}"
-        result = str(self.value['constant']) if self.value['constant'] != 0.0 else ''
+        if self.value['constant'] % 1 == 0.0 and self.value['constant'] != 0.0:
+            result = str(int(self.value['constant']))
+        else:
+            result = str(self.value['constant']) if self.value['constant'] != 0.0 else ''
         for variable in self.value['variables']:
             if variable['coef'] != 0:
-                result += ' + ' + str(variable['coef']) if (variable['coef'] >= 0) else ' - ' + str(variable['coef']*-1)
+                if variable['coef'] == 1:
+                    result += ' + '
+                elif variable['coef'] == -1:
+                    result += ' - '
+                else:
+                    result += ' + ' + str(variable['coef']) if (variable['coef'] >= 0) else ' - ' + str(variable['coef']*-1)
                 for x in variable["name"]:
                     result += f'{x}^' + str(variable['variable'][x]) if variable['variable'][x] != 1 else x
+
         return result
