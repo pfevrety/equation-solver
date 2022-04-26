4f474f77c8dd4f89211375e079e71e91edeaaf99
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-26 19:48:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from lexer import Lexer
 from parser_ import Parser
 from interpreter import Interpreter
 
-text = "x + 19 + y - 21 + x - y - y" #input("calc > ")
+text = "1 + x + y + x + 0 + 2 + x + X + y + y + z" #input("calc > ")
 lexer = Lexer(text)
 tokens = tuple(lexer.generate_tokens)
-print("tokens", tokens)  # Done
+# print("tokens", tokens)  # Done
 parser = Parser(tokens)
 tree = parser.parse()
-print("tree", tree)
+# print("tree", tree)
 interpreter = Interpreter()
-value = interpreter.visit(tree)
-print(value)
+value2 = interpreter.visit(tree)
+
+print(value2)
