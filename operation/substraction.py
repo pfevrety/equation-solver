a98fbaac16e12d341f5fd958cf10a8f675c689c4
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-27 09:52:57 +0200

    Update substraction.py

--- operation/substraction.py
+++ operation/substraction.py
@@ -1,19 +1,15 @@
 from operation.utils.get_communs_variables import get_commun_variables
 from operation.utils.get_name import get_name
+from operation import addition
 
 from values import Number
 def linear(node_a, node_b):
-    commun_variables, _commun_variables = get_commun_variables(node_a, node_b)
-    tmp = {'constant': node_a['constant'] - node_b['constant'], 'variables': []}
 
-    for i in commun_variables:
-        if i[0]['coef'] - i[1]['coef'] != 0:
-            tmp['variables'].append({'name': get_name(i[0]['variable']), 'coef': i[0]['coef'] - i[1]['coef'],'variable': i[0]['variable']})
-    for i in _commun_variables:
-        tmp['variables'].append(i)
+    tmp_node_b = {'constant': -node_b['constant'], 'variables': []}
+    for variable in node_b['variables']:
+        tmp_node_b['variables'].append({'name': variable['name'], 'coef': -variable['coef'], 'variable': variable['variable']})
 
-    return Number(tmp)
-
+    return addition.linear(node_a, tmp_node_b)
 
 def derivate(node_a, node_b):
     pass
