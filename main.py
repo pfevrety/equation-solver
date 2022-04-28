240a05e54e831b2ddc5400670645a88d583ede42
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-28 13:36:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lexer import Lexer
 from parser_ import Parser
 from interpreter import Interpreter
 
-text = "12 + xxxxy*(y*y + x + z) + 2 * y - x" #input("calc > ")
+text = "0+"+" (1-x)(1-x)(1-x)" + "+0" #input("calc > ")
 lexer = Lexer(text)
 tokens = tuple(lexer.generate_tokens)
 #print("tokens", tokens)  # Done
@@ -28,7 +28,7 @@ def test():
         '1 + (2 + 3)': '6',
         '(1 + 2 + 3) + 9.2': '15.2',
         '(1.1+2) + (2 + 3)': '8.1',
-        '(1.1+x) + t + (2 + x + 3)': '6.1 + 2x + t',
+        '(1.1+x) + t + (2 + x + 3)': '6.1 + 2.0x + t',
         '(x+2) + (2 + 3) + v': '7 + x + v',
         '- 9.02 - x + 1.1 - x + x + x + x + y - t + x + o -o + x - x - x- x - x': '-7.92 - x + y - t',
         "-(x -(2)) + 2 - (t)": '4 - x - t',
@@ -40,12 +40,13 @@ def test():
     test_failed = 0
     counter = 0
     for test in tests.keys():
-        counter += 1
         if str(tout_faire(test)) == tests[test]:
             test_passed += 1
         else:
             test_failed += 1
-            print(counter)
+            print(f"{tests[test]} == {str(tout_faire(test))}")
+        counter += 1
+
     
             
     print(f"Test passed: {test_passed}, Test failed: {test_failed}")
