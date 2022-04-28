6cead16646caab11620443e8c4521bba707bd5a7
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-28 11:14:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lexer import Lexer
 from parser_ import Parser
 from interpreter import Interpreter
 
-text = "-(x -(2)) + 2 - (t)" #input("calc > ")
+text = "12 + xxxxy*(y*y + x + z) + 2 * y - x" #input("calc > ")
 lexer = Lexer(text)
 tokens = tuple(lexer.generate_tokens)
 #print("tokens", tokens)  # Done
@@ -11,7 +11,9 @@ tree = parser.parse()
 #print("tree", tree)
 interpreter = Interpreter()
 value2 = interpreter.visit(tree)
-
+print(value2)
+print(value2.generate_latex())
+    
 def tout_faire(text):
     lexer = Lexer(text)
     tokens = tuple(lexer.generate_tokens)
@@ -22,29 +24,31 @@ def tout_faire(text):
 
 
 def test():
-    test1 = '1 + (2 + 3)'
-    print(test1, '=', tout_faire(test1), '== 6')
+    tests = {
+        '1 + (2 + 3)': '6',
+        '(1 + 2 + 3) + 9.2': '15.2',
+        '(1.1+2) + (2 + 3)': '8.1',
+        '(1.1+x) + t + (2 + x + 3)': '6.1 + 2x + t',
+        '(x+2) + (2 + 3) + v': '7 + x + v',
+        '- 9.02 - x + 1.1 - x + x + x + x + y - t + x + o -o + x - x - x- x - x': '-7.92 - x + y - t',
+        "-(x -(2)) + 2 - (t)": '4 - x - t',
+        '2 * x * 1 * 2': '4.0x',
+        "12 + xxxxy*(y + x + z) + 2 * y - x": "12 + y^2x^4 + x^5y + zyx^4 + 2.0y - x"
+    }
+
+    test_passed = 0
+    test_failed = 0
+    counter = 0
+    for test in tests.keys():
+        counter += 1
+        if str(tout_faire(test)) == tests[test]:
+            test_passed += 1
+        else:
+            test_failed += 1
+            print(counter)
     
-    test2 = '(1 + 2 + 3) + 9.2'
-    print(test2, '=', tout_faire(test2), '== 15.2')
-    
-    test3 = '(1.1+2) + (2 + 3)'
-    print(test3, '=', tout_faire(test3), '== 8.1')
-
-    test4 = '(1.1+2) + x'
-    print(test4 , '=', tout_faire(test4), '== 3.1 + x')
-
-    test5 = '(1.1+x) + t + (2 + x + 3)'
-    print(test5, '=', tout_faire(test5), '== 6.1 + 2x + t')
-
-    test6 = '(x+2) + (2 + 3) + v'
-    print(test6, '=', tout_faire(test6), '== 7 + x + v')
-
-    test7 = '- 9.02 - x + 1.1 - x + x + x + x + y - t + x + o -o + x - x - x- x - x'
-    print(test7, '=', tout_faire(test7), '== -7.92 - x + y - t')
-
-    test8 = "-(x -(2)) + 2 - (t)"
-    print(test8, '=', tout_faire(test8), '== 4 - x - t')
+            
+    print(f"Test passed: {test_passed}, Test failed: {test_failed}")
 
 
 test()
