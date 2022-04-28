69c25971de672a0ca3a0260b5f92fbe4eb825bab
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-28 13:36:17 +0200

    Update multiplication.py

--- operation/multiplication.py
+++ operation/multiplication.py
@@ -5,20 +5,21 @@ def linear(node_a, node_b):
     tmp = {'constant': node_a['constant'] * node_b['constant'], 'variables': []}
     
 
-    # OK
+    # Multiplie tout par la constante a
     for variable_b in node_b['variables']:
         if variable_b['coef'] * node_a['constant'] != 0:
             tmp['variables'].append({'name': variable_b['name'], 'coef': variable_b['coef'] * node_a['constant'], 'variable': variable_b['variable']})
 
-
-    #
+    
     for variable_a in node_a['variables']:
+        
         if variable_a['coef'] * node_b['constant'] != 0:
             tmp['variables'].append({'name': variable_a['name'], 'coef': variable_a['coef'] * node_b['constant'], 'variable': variable_a['variable']})
         for variable_b in node_b['variables']:
-            tmp1 = {'name': '', 'coef': variable_a['coef'] * variable_b['coef'], 'variable': variable_b['variable']}
+            tmp1 = {'name': '', 'coef': variable_a['coef'] * variable_b['coef'], 'variable': variable_b['variable'].copy()}
             var_b = variable_b['variable'].keys()
             for var in variable_a['variable'].keys():
+
                 if var in var_b:
                     tmp1['variable'][var] += variable_a['variable'][var]
                 else:
