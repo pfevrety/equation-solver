0198f86f13715fb3eb30ad5f9fc28786754e42a7
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-04-28 11:15:18 +0200

    Update values.py

--- values.py
+++ values.py
@@ -19,4 +19,29 @@ class Number:
                 for x in variable["name"]:
                     result += f'{x}^' + str(variable['variable'][x]) if variable['variable'][x] != 1 else x
 
+        if result.startswith(" + "):
+            return result[3:]
         return result
+
+    def generate_latex(self):
+        latex = ''
+        if self.value['constant'] % 1 == 0.0 and self.value['constant'] != 0.0:
+            latex = str(int(self.value['constant']))
+        else:
+            latex = str(self.value['constant']) if self.value['constant'] != 0.0 else ''
+
+        for variable in self.value['variables']:
+            if variable['coef'] != 0:
+                if variable['coef'] == 1:
+                    latex += ' + '
+                elif variable['coef'] == -1:
+                    latex += ' - '
+                else:
+                    latex += ' + ' + str(variable['coef']) if (variable['coef'] >= 0) else ' - ' + str(variable['coef']*-1)
+                for x in variable["name"]:
+                    if x != variable["name"][-1]:
+                        latex += x+'^{' + str(variable['variable'][x]) + '}*' if variable['variable'][x] != 1 else x + '*'
+                    else:
+                        latex += x+'^{' + str(variable['variable'][x]) + '}' if variable['variable'][x] != 1 else x
+
+        return latex
