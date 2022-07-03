eb046107725c5a41db6e2a3eb577f7bf7f73286c
pfevrety <60936445+pfevrety@users.noreply.github.com>
2022-07-03 13:25:04 +0200

    Update is_the_same.py

--- operation/utils/is_the_same.py
+++ operation/utils/is_the_same.py
@@ -1,7 +1,7 @@
 def is_the_same(var1, var2):
-    if var1['name'] == var2['name']:
-        for var in var1['variable'].keys():
-            if var1['variable'][var] != var2['variable'][var]:
+    if var1.name == var2.name:
+        for var in var1.variables.keys():
+            if var1.variables[var] != var2.variables[var]:
                 return False
         return True
     else:
